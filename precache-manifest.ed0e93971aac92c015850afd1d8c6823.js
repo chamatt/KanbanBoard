3572957a0d5296211e1b6c05a4fd20be5e35cc39
ChaMatt <matt.vicent@gmail.com>
2020-05-09 06:16:59 -0300

    Updates

---
+++ precache-manifest.ed0e93971aac92c015850afd1d8c6823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda6d67d742870988367334068462e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe334e3595ef804a659a",
+    "url": "/static/css/2.2bd37e2f.chunk.css"
+  },
+  {
+    "revision": "fe334e3595ef804a659a",
+    "url": "/static/js/2.953ef503.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.953ef503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dcecdbd75bee3ff073",
+    "url": "/static/js/main.4c61e89c.chunk.js"
+  },
+  {
+    "revision": "3a212fd449a5aee36cee",
+    "url": "/static/js/runtime-main.8a11f9eb.js"
+  }
+]);
