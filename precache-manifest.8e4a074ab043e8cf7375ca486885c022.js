de46ba5afd1af55c4bb60a639ac60f7b69fb17d1
ChaMatt <matt.vicent@gmail.com>
2020-05-10 16:56:01 -0300

    Updates

---
+++ precache-manifest.8e4a074ab043e8cf7375ca486885c022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda622917abd237aacdd101fcd132924",
+    "url": "/tasks-kaban-board/index.html"
+  },
+  {
+    "revision": "06c93819449f611dd7a9",
+    "url": "/tasks-kaban-board/static/css/main.665f99f1.chunk.css"
+  },
+  {
+    "revision": "5355f6b8f8efd028cdf0",
+    "url": "/tasks-kaban-board/static/js/2.de5f9804.chunk.js"
+  },
+  {
+    "revision": "4f4b39e776c4ce087d84114e0d382cdf",
+    "url": "/tasks-kaban-board/static/js/2.de5f9804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c93819449f611dd7a9",
+    "url": "/tasks-kaban-board/static/js/main.11f0193c.chunk.js"
+  },
+  {
+    "revision": "b78aa0875ea6fb80dd2f",
+    "url": "/tasks-kaban-board/static/js/runtime-main.81297170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tasks-kaban-board/precache-manifest.959ac220cdab0cd04260717ccea68beb.js"
+  "/tasks-kaban-board/precache-manifest.8e4a074ab043e8cf7375ca486885c022.js"
 );
 
 self.addEventListener('message', (event) => {
